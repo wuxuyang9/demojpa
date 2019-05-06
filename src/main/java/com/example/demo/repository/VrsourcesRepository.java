117139f21c3eed7ec2c63016be27bd17a5395601
wuxuyang9 <goodwuxuyang@foxmail.com>
2019-05-06 21:39:49 +0800

    jpa

---
+++ src/main/java/com/example/demo/repository/VrsourcesRepository.java
@@ -0,0 +1,10 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.VrsourcesEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by goodwuxuyang on 2019/5/6.
+ */
+public interface VrsourcesRepository extends JpaRepository<VrsourcesEntity,Integer> {
+}
---
+++ src/test/java/com/example/demo/DemojpaApplicationTests.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import com.example.demo.repository.VrsourcesRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemojpaApplicationTests {
+
+
+	@Autowired
+	private VrsourcesRepository repository;
+	@Test
+	public void contextLoads() {
+//		repository.findAll().forEach(System.out::println);
+
+
+	}
+
+}
