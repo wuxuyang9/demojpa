71b4e548b456d054f9caa36ac13126624a76c02c
lrving <goodwuxuyang@foxmail.com>
2020-07-11 23:10:38 +0800

    test

---
+++ src/test/java/com/example/demo/service/impl/CarUpkeepServiceImplTest.java
@@ -0,0 +1,13 @@
+package com.example.demo.service.impl;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CarUpkeepServiceImplTest {
+
+    @Test
+    public void save() {
+        System.out.println("tset");
+    }
+}
