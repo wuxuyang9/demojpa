d41ffbe88521e00cb73ef62c9a324a66436371e8
lrving <goodwuxuyang@foxmail.com>
2020-07-19 21:17:52 +0800

    ToWork

---
+++ src/main/java/com/example/demo/entity/vo/ToWork.java
@@ -0,0 +1,11 @@
+package com.example.demo.entity.vo;
+
+/**
+ * @ClassName ToWork
+ * @Description TODO
+ * @Author goodwuxuyang
+ * @Date 2020/7/19 21:17
+ * @Version 1.0
+ **/
+public class ToWork {
+}
