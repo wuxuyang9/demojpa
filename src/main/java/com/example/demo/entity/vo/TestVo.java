a0edcfa3e7f892e95b22b0ea3247ab03dbbaf938
lrving <goodwuxuyang@foxmail.com>
2020-07-19 21:16:57 +0800

    TestVo

---
+++ src/main/java/com/example/demo/entity/vo/TestVo.java
@@ -0,0 +1,11 @@
+package com.example.demo.entity.vo;
+
+/**
+ * @ClassName TestVo
+ * @Description TODO
+ * @Author goodwuxuyang
+ * @Date 2020/7/19 21:16
+ * @Version 1.0
+ **/
+public class TestVo {
+}
